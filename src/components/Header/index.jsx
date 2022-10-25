6518e31c0720f42e2dfb1dd0d5dabcef6af86a56
Pranav Bakre <pranavbakre@yahoo.in>
2022-10-25 15:50:00 +0530

    feat: Using customnav link in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,18 @@
 import React from "react"
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
+import NavLink from "components/CustomNavLink";
 import "./index.css"
 
-const buttonStyle = (isActive) => ({textDecoration: "none",
-padding: "1rem 2rem", color: "inherit", ...(isActive && {backgroundColor: "rgba(255,255,255,0.1)"})})
+const buttonStyle = (to, location) => ({textDecoration: "none",
+padding: "1rem 2rem", color: "inherit", ...((to === location.hash) && {backgroundColor: "rgba(255,255,255,0.1)"})})
 
+const homeButtonStyle = (to, location) => ({textDecoration: "none",
+padding: "1rem 2rem", color: "inherit", ...((to === location.hash || location.hash === "") && {backgroundColor: "rgba(255,255,255,0.1)"})})
 export function Header (){
     return (
         <div className="header-style">
-            <NavLink to={"/#home"} style={buttonStyle} >Home</NavLink>
-            <NavLink to={{pathname: "/", hash: "about"}} style={buttonStyle} >About</NavLink>
+            <NavLink to={"#home"} style={homeButtonStyle} custom >Home</NavLink>
+            <NavLink to={"#about"} style={buttonStyle} custom >About</NavLink>
         </div>
     )
 }
